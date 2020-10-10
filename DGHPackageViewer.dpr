6f89472c9f0590c6aad2c2ff9f97d2286332fb1e
David Hoyle <davidghoyle@gmail.com>
2020-10-10 14:10:54 +0100

    Updated the license file and source code headers for GPL 3

--- DGHPackageViewer.dpr
+++ DGHPackageViewer.dpr
@@ -5,7 +5,26 @@
 
   @Author  David Hoyle
   @Version 1.029
-  @Date    09 Oct 2020
+  @Date    10 Oct 2020
+
+  @license
+
+    Package Viewer is a RAD Studio plug-in for browsing packages loaded into the IDE.
+    
+    Copyright (C) 2020  David Hoyle (https://github.com/DGH2112/Browse-and-Doc-It/)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
   @nocheck EmptyBeginEnd
   
